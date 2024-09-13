622d91edf4c439ea1511673ee1370035f264da25
o <o@dmstfctn.net>
2024-09-13 09:39:03 +0100

    enable using query string ?pane=paneid to show a panel to make it easier to design/fill content

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import '/src/interface.js';
 import '/src/videoControls.js';
+
+window.location.search.replace('?','').split('&').forEach( (entry) => { 
+    const [cmd, val] = entry.split('=');
+    if( cmd === 'pane' ){
+        document.getElementById( val ).style.opacity = 1;
+    }
+})
