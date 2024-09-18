7c07859c6997f88fdd8f8b11ff58cd22f65c81df
o <o@dmstfctn.net>
2024-09-18 11:26:12 +0100

    add close button to panels, wrap to still allow scroll, fix some sizing issues

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import '/src/videoControls.js';
 window.location.search.replace('?','').split('&').forEach( (entry) => { 
     const [cmd, val] = entry.split('=');
     if( cmd === 'pane' ){
-        document.getElementById( val ).style.opacity = 1;
+        //document.getElementById( val ).style.opacity = 1;
+        document.getElementById( val ).classList.add('visible');
         document.getElementById( val ).querySelectorAll('img, video').forEach(( $ele ) => { 
             $ele.src = $ele.dataset.src;
         });
